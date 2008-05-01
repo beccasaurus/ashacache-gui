7ea4760b0a84b76b63c96c364a774a0c70fa2cbf
remi <remi@remitaylor.com>
2008-05-01 05:12:40 -0700

    added gitignore ... got very insanely basic ActiveResource working ... because of a lack of access to some things via REST, i'm using hpricot/open-uri

--- Rakefile
+++ Rakefile
@@ -5,9 +5,12 @@ namespace :gems do
   desc 'Install Required Gems'
   task :install do
     sudo = (RUBY_PLATFORM[/windows/i]) ? nil : 'sudo'
-    gems = %w( remi-simplecli ).join ' '
-    puts "Installing gems ... ( please be patient )"
-    puts `#{ sudo } gem install #{ gems } --source http://gems.github.com`
+    
+    puts "Installing gems from rubyforge ... ( please be patient )"
+    puts `#{ sudo } gem install activeresource`
+
+    puts "Installing gems from github ... ( please be patient )"
+    puts `#{ sudo } gem install remi-simplecli --source http://gems.github.com`
   end
 
 end
--- lib/ashacache.rb
+++ lib/ashacache.rb
@@ -1,5 +1,53 @@
 $:.unshift File.dirname(__FILE__)
 
-%w( rubygems ).each { |lib| require lib }
+%w( rubygems activeresource ).each { |lib| require lib }
 
-module Ashacache; end
+SITE, USER, PASS = "http://ashacache.com", "John Smith", "*******"
+ActiveResource::Base.site = "http://#{USER}:#{PASS}@ashacache.com"
+
+module Ashacache
+
+  def doc path
+    require 'hpricot'
+    Hpricot html(path)
+  end
+  def html path
+    require 'open-uri'
+    domain = (path['http://']) ? nil : SITE
+    open "#{domain}#{path}", :http_basic_authentication => [USER, PASS]
+  end
+
+  class Member < ActiveResource::Base
+    def hunts
+      Hunt.find(:all).select { |h| h.member_id == id }
+    end
+    def self.find_by_name name
+      Member.find(:all).find { |m| m.name == name }
+    end
+  end
+
+  class Hunt < ActiveResource::Base
+    def member
+      @member ||= Member.find member_id
+    end
+    def image
+      @image ||= SITE + ( doc("/hunts/#{id}") / 'div#showHuntImageCol>img ').first['src']
+    end
+
+    def local_image
+      require 'fileutils'
+      dir  = File.join 'tmp', File.dirname(image).sub('http://','')
+      file = File.join dir, File.basename(image)
+      FileUtils.mkdir_p dir unless File.directory? dir
+      FileUtils.mv html(image).path, file unless File.file? file
+      file
+    end
+  end
+
+  # Comments are not currently available
+  class Comment; end
+
+end
+
+# for testing
+include Ashacache
